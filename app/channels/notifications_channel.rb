e1c99822be1a4b4bfe143e34915bace6f34d14ab
letItCurl <rolandlopez.developer@gmail.com>
2020-10-31 16:07:45 +0700

    printed to the console

--- app/channels/notifications_channel.rb
+++ app/channels/notifications_channel.rb
@@ -1,4 +1,8 @@
 class NotificationsChannel < ApplicationCable::Channel
+
+  # in job to broadcas to every one
+  # ApplicationController.render partial: "notifications/#{n.notifiable_type.underscore.pluralize}/#{n.action}", locals: {notification: n}, formats: [:html]
+
   def subscribed
     stream_from "notifications:#{current_user.id}"
   end
