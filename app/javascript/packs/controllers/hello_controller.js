46fa25fc4b401cd9c42d6d44ee88ebeccdd9731e
letItCurl <rolandlopez.developer@gmail.com>
2020-11-01 19:15:18 +0700

    state

--- app/javascript/packs/controllers/hello_controller.js
+++ app/javascript/packs/controllers/hello_controller.js
@@ -1,8 +1,30 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller{
+  initialize(){
+    this.nameEl.value = this.name
+  }
+
   log(){
     console.log(this.targets)
-    console.log(this.targets.find("name").value)
+    console.log(this.nameEl.value)
+  }
+
+  paste(event){
+    event.preventDefault()
+    console.log("paste not allowed")
+  }
+
+  get name(){
+    console.log(this.data)
+    if(this.data.has("name")){
+      return this.data.get("name")
+    } else {
+      return "Default value"
+    }
+  }
+
+  get nameEl(){
+    return this.targets.find("name")
   }
 }
